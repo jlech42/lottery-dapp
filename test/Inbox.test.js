79c1be078f04e945bc8d93506ec717c2c3d688e0
Justin Lechner <jlech42@gmail.com>
2018-06-19 19:26:20 -0400

    Test call message function

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider()); // Web3 instance for local test network
+const provider = ganache.provider();
+const web3 = new Web3(provider); // Web3 instance for local test network
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there';
 
 beforeEach(async () => {
   // Get all accounts
@@ -13,12 +15,19 @@ beforeEach(async () => {
 
   // Use account to deploy contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there'] })
-    .send({ from: accounts[0], gas: '1000000' })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('verify initial message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
   });
 });
