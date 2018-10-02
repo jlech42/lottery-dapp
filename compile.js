9aa4c3f4c7e59a792c67e9e536c46ada700d1a19
Justin Lechner <jlech42@gmail.com>
2018-10-02 14:57:01 -0400

    test case for entering contract

--- compile.js
+++ compile.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const solc = require('solc');
 
 // Smart contract path
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 
 // Read source code
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 // Compile & export Inbox contract
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile')
+const { interface, bytecode } = require('./compile');
 
 //const API_KEY = 'OUQt3b4pfjvmKFEuBqOs';
 
@@ -13,9 +13,9 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('attempting to deploy from account',accounts[0]);
+  console.log('attempting to deploy from account', accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: '0x' + bytecode })
     .send({ gas: '1000000', from: accounts[0] });
   console.log('contract deployed to', result.options.address);
 };
